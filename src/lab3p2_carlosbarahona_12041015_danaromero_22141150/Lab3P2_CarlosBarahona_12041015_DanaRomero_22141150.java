82ef66355686307c3b4dd819595c4237f9102ff6
dromero000 <d.romero@unitec.edu>
2022-02-04 13:48:02 -0600

    Primer commit

---
+++ src/lab3p2_carlosbarahona_12041015_danaromero_22141150/Lab3P2_CarlosBarahona_12041015_DanaRomero_22141150.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3p2_carlosbarahona_12041015_danaromero_22141150;
+
+/**
+ *
+ * @author Dana Romero
+ */
+public class Lab3P2_CarlosBarahona_12041015_DanaRomero_22141150 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
